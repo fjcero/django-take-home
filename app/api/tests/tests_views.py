0956f3f3f38d44f270f2d8493ad1de2e66436586
Francisco Caballero <fjcero@gmail.com>
2022-04-03 16:34:09 -0300

    feat: Add initial tests for incoming webhooks

--- app/api/tests/tests_views.py
+++ app/api/tests/tests_views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
 from .factories import PatientFactory
+
 # Required to enable SkipTest
 # import unittest
 
@@ -8,21 +9,23 @@ from .factories import PatientFactory
 class APITest(APITestCase):
     def test_get_patient(self):
         patient_mocks = PatientFactory.create_batch(4)
-        response = self.client.get('/patient/')
+        response = self.client.get("/patient/")
         self.assertEquals(response.status_code, status.HTTP_200_OK)
         self.assertEquals(len(response.json()), 4)
 
     def test_create_patient(self):
         # Create assertable responses
         error_response = self.client.post(
-            '/patient/',
-            data=dict(email="", name=""),
-            format='json'
+            "/patient/", data=dict(email="", name=""), format="json"
         )
         valid_response = self.client.post(
-            '/patient/',
+            "/patient/",
             data=dict(email="sally@floatfi.com", name="Sally Jones"),
-            format='json'
+            format="json",
         )
         self.assertEquals(error_response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEquals(valid_response.status_code, status.HTTP_201_CREATED)
+
+    def test_ehr_webhook(self):
+        status_code = 201
+        self.assertEquals(status_code, status.HTTP_201_CREATED)
